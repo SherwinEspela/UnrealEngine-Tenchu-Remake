33a89d2460ab98d4187d630edbda012a021d954c
Sherwin Espela <sherwinjai_espela@yahoo.ca>
2023-10-19 14:12:41 -0700

    added v5.2 unreal project

---
+++ TenchuFanRemake/Source/TenchuFanRemake/TenchuFanRemakeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TenchuFanRemakeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TENCHUFANREMAKE_API ATenchuFanRemakeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
