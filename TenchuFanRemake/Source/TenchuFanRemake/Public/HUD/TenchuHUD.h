5a5bd7f890d459164b4daef8e34bcc8f7afcc68f
Sherwin Espela <sherwinjai_espela@yahoo.ca>
2023-11-05 14:17:39 -0800

    Adding Image assets for Enemy Detector Widget

---
+++ TenchuFanRemake/Source/TenchuFanRemake/Public/HUD/TenchuHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "TenchuHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TENCHUFANREMAKE_API ATenchuHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
